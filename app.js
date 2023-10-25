ae94a42b403b58534449349994399e98fdb14b4f
Ricardo <rvardo98@gmail.com>
2023-10-25 00:58:56 -0500

    customizing grid size

--- app.js
+++ app.js
@@ -1,34 +1,60 @@
 const bodyElement = document.getElementById("body");
 const btn = document.getElementById("btn");
+const gridContainer = document.getElementById("container");
 
-for (let i = 0; i < 26; i++) {
-  const div = document.createElement("div");
-  div.classList.add("box-container");
-  for (let j = 0; j < 26; j++) {
-    // console.log(i, j);
-    const insideDiv = document.createElement("div");
-    insideDiv.classList.add("box");
-    div.appendChild(insideDiv);
+function createGrid(rows, columns) {
+  for (let i = 0; i < rows; i++) {
+    const div = document.createElement("div");
+    div.classList.add("box-container");
+    for (let j = 0; j < columns; j++) {
+      // console.log(i, j);
+      const insideDiv = document.createElement("div");
+      insideDiv.classList.add("box");
+      div.appendChild(insideDiv);
+    }
+
+    gridContainer.appendChild(div);
   }
-
-  bodyElement.appendChild(div);
 }
+
+createGrid(16, 16);
 
 const boxes = document.querySelectorAll(".box");
 
-// boxes.forEach((box) => {
-//   box.addEventListener("mouseover", (e) => {
-//     // box.className.add("color");
-//     console.log(e.target.className);
-//     e.target.style.backgroundColor = "red"; // Apply the hover effect
-//   });
-// });
-
-console.log(boxes);
+boxes.forEach((box) => {
+  box.addEventListener("mouseover", (e) => {
+    // box.className.add("color");
+    console.log(e.target.className);
+    e.target.style.backgroundColor = "red"; // Apply the hover effect
+  });
+  console.log("inside for each!");
+});
 
 btn.addEventListener("click", () => {
-  const userColumns = prompt("select the amount of columns you want:");
-  const userRows = prompt("select the amount of rows you want:");
+  let columnsNum;
+  let rowsNum;
 
-  
+  while (true) {
+    columnsNum = parseInt(prompt("select the number of columns you want:"));
+    rowsNum = parseInt(prompt("select the number of rows you want:"));
+
+    if (!isNaN(columnsNum && rowsNum) && columnsNum < 50 && rowsNum < 50) {
+      break;
+    } else {
+      alert("Please insert a number (must be less than 100)");
+    }
+  }
+
+  while (gridContainer.children.length > 0) {
+    gridContainer.removeChild(gridContainer.children[0]);
+  }
+
+  createGrid(rowsNum, columnsNum);
+  console.log("inside click listener");
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "red"; // Apply the hover effect
+    });
+  });
 });
